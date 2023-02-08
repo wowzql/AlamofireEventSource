0a4a3021a98c9bbf70180f12486e42c986a8dfa0
SUNG <wowzql@gmail.com>
2023-02-08 22:37:53 +0800

    Update Alamofire+EventSource.swift

--- Sources/AlamofireEventSource/Alamofire+EventSource.swift
+++ Sources/AlamofireEventSource/Alamofire+EventSource.swift
@@ -10,11 +10,36 @@ import Alamofire
 
 extension Session {
     
-    public func eventSourceRequest(_ convertible: URLConvertible, method: HTTPMethod = .get, headers: HTTPHeaders? = nil, lastEventID: String? = nil) -> DataStreamRequest {
-        return streamRequest(convertible, headers: headers) { request in
+    struct RequestConvertible: URLRequestConvertible {
+        let url: URLConvertible
+        let method: HTTPMethod
+        let parameters: Parameters?
+        let encoding: ParameterEncoding
+        let headers: HTTPHeaders?
+        let requestModifier: RequestModifier?
+
+        func asURLRequest() throws -> URLRequest {
+            var request = try URLRequest(url: url, method: method, headers: headers)
+            try requestModifier?(&request)
+
+            return try encoding.encode(request, with: parameters)
+        }
+    }
+    
+    public func eventSourceRequest(_ convertible: URLConvertible, method: HTTPMethod = .get, parameters: Parameters? = nil, headers: HTTPHeaders? = nil, encoding: ParameterEncoding = URLEncoding.default , requestModifier: RequestModifier? = nil, interceptor: RequestInterceptor? = nil, lastEventID: String? = nil) -> DataStreamRequest {
+        
+        let convertible = RequestConvertible(url: convertible,
+                                             method: method,
+                                             parameters: parameters,
+                                             encoding: encoding,
+                                             headers: headers,
+                                             requestModifier: requestModifier)
+        
+        return streamRequest(convertible as! URLConvertible, interceptor: interceptor) { request in
             request.timeoutInterval = TimeInterval(Int32.max)
             request.headers.add(name: "Accept", value: "text/event-stream")
             request.headers.add(name: "Cache-Control", value: "no-cache")
+            
             if let lastEventID = lastEventID {
                 request.headers.add(name: "Last-Event-ID", value: lastEventID)
             }
